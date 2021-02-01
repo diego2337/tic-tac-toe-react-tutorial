ef2118db70b220c5fee8d69ec9c6387cfd53214d
diego2337 <diegosc2337@gmail.com>
2021-02-01 11:19:35 -0300

    feat: adding (row, col) notation in move history

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,6 +1,31 @@
 import React from 'react';
-import { Square } from './Square';
+import Square from './Square';
 export default class Board extends React.Component {
+  mapIndexToRowCol(index) {
+    switch (index) {
+      case 0:
+        return "(0, 0)";
+      case 1:
+        return "(0, 1)";
+      case 2:
+        return "(0, 2)";
+      case 3:
+        return "(1, 0)";
+      case 4:
+        return "(1, 1)";
+      case 5:
+        return "(1, 2)";
+      case 6:
+        return "(2, 0)";
+      case 7:
+        return "(2, 1)";
+      case 8:
+        return "(2, 2)";
+      default:
+        return "";  
+    }
+  }
+
   calculateWinner(squares) {
     const lines = [
       [0, 1, 2],
--- src/components/Square.js
+++ src/components/Square.js
@@ -1,7 +1,10 @@
-export function Square(props) {
+import React from 'react';
+export default class Square extends React.Component {
+  render() {
     return (
-      <button className="square" onClick={props.onClick}>
-        {props.value}
+      <button className="square" onClick={this.props.onClick}>
+        {this.props.value}
       </button>
     );
   }
+}
